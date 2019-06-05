79f74ffcd1f55b40e32a97f1fe0f1367930e05a0
alexandre <acuenca@quotemedia.com>
2019-06-05 00:49:55 -0700

    moved cache logic to model

--- app/models/concerns/eve_data.rb
+++ app/models/concerns/eve_data.rb
@@ -2,16 +2,34 @@ module EveData
   extend ActiveSupport::Concern
 
   def self.region_history(params)
-    Rails.logger.info("EveDat.region_history : " + params.to_s)
+    Rails.logger.info("EveData.region_history : " + params.to_s)
+    @region = Region.find_by!(eve_id: params[:region_id])
+    hist = @region.histories.find_by(item: params[:id])
+
+    threshold = Date.today.prev_day(2)
+    if hist && (hist.updated_at <=> threshold) == 1
+      return hist
+    else
+      History.create_from_hist(params, hit_eve(eve_params(params)))
+    end
+  end
+
+  def self.hit_eve(params)
+    Rails.logger.info("EveData.hit_eve : " + params.to_s)
     response = Faraday.get("https://esi.evetech.net/latest/markets/#{params[:region_id]}/history/?", {type_id: params[:type_id]})
     if response.success?
       JSON.parse(response.body).map {|hist| create_hist(hist) }.to_a
     else
-      Rails.logger.error("https://esi.evetech.net/latest/markets/" + status.to_s + "/" + response.body)
-      raise NotFoundError
+      Rails.logger.error("https://esi.evetech.net/latest/markets/" + response.status.to_s + "/" + response.body)
+      raise "Eve server does not have the resource : " + params.to_s
     end
   end
+  
+  def self.eve_params(params)
+    { region_id: params[:region_id], type_id: params[:item] }
+  end
 
+  
   def self.create_hist(hist)
     OpenStruct.new(average: hist["average"],
                    date: hist["date"],
