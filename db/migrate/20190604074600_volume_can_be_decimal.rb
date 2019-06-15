240a5481e5ed683681aabcb560b3f057277be79b
alexandre <acuenca@quotemedia.com>
2019-06-15 01:31:11 -0700

    fixed migration

--- db/migrate/20190604074600_volume_can_be_decimal.rb
+++ db/migrate/20190604074600_volume_can_be_decimal.rb
@@ -1,6 +1,5 @@
 class VolumeCanBeDecimal < ActiveRecord::Migration[5.2]
   def change
-    remove_column :histories, :modified_at
     change_column :histories, :median, :float
     change_column :histories, :quantity, :float
   end
