72bc2fffd53a49d2434d90523de7240e78eda36a
alexandre <acuenca@quotemedia.com>
2019-06-16 09:36:53 -0700

    remove debug in prod

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.logger = Logger.new(STDOUT)
+#Rails.logger = Logger.new(STDOUT)
 #config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
-Rails.logger.level = Logger::DEBUG
+
+#Rails.logger.level = Logger::DEBUG
 Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
